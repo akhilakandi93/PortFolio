a4402c084389727f36f952a0f545e00a54244f1d
Akhila Kandi <akandi7227@gmail.com>
2017-11-06 22:57:52 -0500

    first commit

---
+++ myScript.js
@@ -0,0 +1,33 @@
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+	scrollFunction()
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("myBtn").style.display = "block";
+	} else {
+		document.getElementById("myBtn").style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+function initMap() {
+	var uluru = {
+		lat: 40.7280556,
+		lng: -74.0780556
+	};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 15,
+		center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}
